c5be7a3c470bf9d50287b62306ebcfa5ed1fdcf0
Your Name <you@example.com>
2022-03-01 16:04:57 +0100

    plugin megirva

--- wp-content/plugins/test/test.php
+++ wp-content/plugins/test/test.php
@@ -6,6 +6,16 @@
 
  function test_send_mail($post) {
      //mail(...)
+
+     error_log("---------------");
      error_log("Üzenet");
+     error_log("---------------");
  }
- 
+ add_action("save_post", "test_send_mail", 10, 3);
+
+ function test_delete_post(int $postid, WP_Post $post) {
+    error_log("---------------");
+    error_log("Törölve: {$post->post_title}");
+    error_log("---------------");
+ }
+ add_action("delete_post", "test_delete_post", 10, 2);
